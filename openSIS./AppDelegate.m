3046b5e3a788e5688a1a1469171d850c6ccf8b07
os4ed <os4ed@os4eds-mac-mini.local>
2016-09-05 17:21:54 +0530

    jkhjkhj

--- openSIS./AppDelegate.m
+++ openSIS./AppDelegate.m
@@ -5,7 +5,7 @@
 //  Created by EjobIndia on 10/08/15.
 //  Copyright (c) 2015 openSiS. All rights reserved.
 //
-
+//sdjfhjksdafjkasdhfkahsfkjhasdkfjgasdkjfgdasjkfgjadf
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
