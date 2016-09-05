ea093c90375559bdf2dd5c7ea7352fa1829863ea
EjobIndia <ejobindia@ejobindias-mac-mini.local>
2016-09-05 18:37:24 +0530

    iiii

--- openSIS./AppDelegate.h
+++ openSIS./AppDelegate.h
@@ -4,13 +4,7 @@
 //
 //  Created by EjobIndia on 10/08/15.
 //  Copyright (c) 2015 openSiS. All rights reserved.
-//amieii--
-//yyyyyyyyyy
-//rrrr yyyyyyyyyyyyyy tfgttttt
-//whd
-//yyyyyrfrrere
-//rrrrrrrrr
-//pppp
+
 
 #import <UIKit/UIKit.h>
 #import "ViewController.h"
